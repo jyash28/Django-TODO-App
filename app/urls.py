c5303e3c14af4399cc1d7c6bd2fd846a4cb1224e
jyash28 <jyash548@gmail.com>
2021-04-03 17:44:46 +0530

    first-commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import HttpResponse
+
+
+def home(request):
+    print("hello")
+    return HttpResponse("hello world")
+
+urlpatterns = [
+    path('',home)
+]
