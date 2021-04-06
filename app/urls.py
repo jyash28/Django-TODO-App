4a327f9c5745e5307f109f000df229ea60fd39d3
jyash28 <jyash548@gmail.com>
2021-04-06 22:09:13 +0530

    update

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.shortcuts import HttpResponse
 
 def home(request):
     print("hello")
-    return HttpResponse("hello world")
+    return HttpResponse("hello world")           
 
 urlpatterns = [
     path('',home)
